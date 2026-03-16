810633f3ac72ff694a1b80a8cc5beab2d16df5d3
Huzoor laghari <huzoorbuxlsghari>
2026-03-16 11:02:38 +0500

    first commit

---
+++ even/odd.py
@@ -0,0 +1,10 @@
+"""num = int(input("number likho: "))
+if num % 2 == 0:
+    print(" even number")
+else:
+    print("odd number")"""
+
+
+for i in range ( 1, 6):
+    print(i)
+
---
+++ test.py
@@ -0,0 +1,7 @@
+name = "huzoor"
+age = 22
+price = 23.22
+
+age2 = age
+
+print(age2)
